09981f8334f327ef95810170208868d7552cf820
Aditya <71590571+adityapratap12@users.noreply.github.com>
2020-11-25 20:48:08 +0530

    Add files via upload

---
+++ negetivefirst.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 25 20:26:40 2020
+
+@author: 91788
+"""
+
+
+def negfirst(arr,n):
+    j=0
+    for i in range (0,n):
+        if(arr[i]<0):
+            temp=arr[i]
+            arr[i]=arr[j]
+            arr[j]=temp
+            j=j+1 
+    print(arr)
+arr=[] 
+n=int(input("enter the value of n"))
+for i in range (0,n):
+    ese=int(input())
+    arr.append(ese)
+negfirst(arr, n)
